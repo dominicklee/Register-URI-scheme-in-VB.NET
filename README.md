f5c2644f2c073ad4f763558b551f58cd8013792a
Dominick Lee <dnicky2288@yahoo.com>
2021-01-23 19:16:17 -0800

    Added code and updated README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Register-URI-scheme-in-VB.NET
+# Register URI scheme in VB.NET
 Basic example of how to register a new URI scheme in VB.NET
+
+## Overview ##
+As we have seen over the years, there are many applications such as Skype, Zoom, Leap Motion, Postman, etc that use URI schemes to convey information from web hyperlinks to an application on a computer. This is often helpful because data can be relayed to a computer with a specific application. Today, we are going to demonstrate an example of that using VB.NET on a Windows PC.
+
+
+
+## Usage ##
+1. Download this repository and open the solution.
+2. Rename the `UriScheme` to whatever "protocol" you want people to type in the browser. Rename `FriendlyName` to the formal name of your application.
+3. Run the `RegisterUriScheme()` function to add your app in the registry. You can create a setting to run this the first time.
+4. Run the `UnegisterUriScheme()` function to remove your app from the registry. You should allow users to run this function in case they want to uninstall your app.
+5. On form startup, you can get the string from `getParams()`. If the string is not blank, then someone started your app from the URI schema.
+
+## Tips ##
+If you plan to use this method in a production setting, ensure that you provide HTTP link to a page prior to providing the user the schema link to help the end-user understand what is happening. Some browsers such as Chrome warns users prior to them opening an application.
+
+Additionally, do **not** use this method to convey directories for file/folder modification, creation, or deletion. This will introduce security vulnerabilities to hackers. Even if your own links or app is not malicious, hackers will have a means to breach computers that run your application.
---
+++ URI Schema Example/URI Schema Example/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+    Const UriScheme As String = "myapp"
+    Const FriendlyName As String = "My App"
+
+    Public Shared Sub RegisterUriScheme()
+        Using key = Registry.CurrentUser.CreateSubKey("SOFTWARE\Classes\" & UriScheme)
+            Dim applicationLocation As String = Application.ExecutablePath
+            key.SetValue("", "URL:" & FriendlyName)
+            key.SetValue("URL Protocol", "")
+
+            Using defaultIcon = key.CreateSubKey("DefaultIcon")
+                defaultIcon.SetValue("", applicationLocation & ",1")
+            End Using
+
+            Using commandKey = key.CreateSubKey("shell\open\command")
+                commandKey.SetValue("", """" & applicationLocation & """ ""%1""")
+            End Using
+        End Using
+    End Sub
+
+    Public Shared Sub UnregisterUriScheme()
+        If (UriScheme <> "") Then   'for safety since if UriScheme is blank, we would delete everything!
+            Registry.CurrentUser.DeleteSubKeyTree("SOFTWARE\Classes\" & UriScheme)
+        End If
+    End Sub
+
+    Public Shared Function getParams()
+        'Parses and returns the info sent from URI scheme
+        Dim args As String() = Environment.GetCommandLineArgs()
+        Dim param As String = ""
+
+        If args.Length > 0 Then
+            For Each arg In args
+                If arg.StartsWith(UriScheme & "://") Then
+                    param = arg.Replace(UriScheme & "://", "")
+                    Exit For
+                End If
+            Next
+        End If
+        Return param
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim text As String = getParams()
+        If (text <> "") Then
+            MsgBox(text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        RegisterUriScheme()
+        MsgBox("App registered!")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        UnregisterUriScheme()
+        MsgBox("App unregistered!")
+    End Sub
+End Class
